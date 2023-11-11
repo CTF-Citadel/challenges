893ec4e5005f5df8dbaac93e9306b09d68405297
Maximilian Birnbacher <66524685+maxbirnbacher@users.noreply.github.com>
2023-11-11 14:26:00 +0100

    fixed converter.sh
    Now modifies to pcap file correctly

--- Pcap_Pandemonium/converter.sh
+++ Pcap_Pandemonium/converter.sh
@@ -9,22 +9,7 @@ output_file="./capture.pcapng"
 # Generate a UUID with the format "TH{xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}"
 new_password="TH{$(python3 -c 'import uuid; print(str(uuid.uuid4()))' | tr -d '-' | tr '[:upper:]' '[:lower:]')}"
 
-# Convert the pcapng file to a hex dump
-xxd -p $input_file > temp_hex_dump.txt
+# Replace the password in the pcapng file and remove the trailing "test"
+awk -v new_pass="$new_password" 'BEGIN {FS=OFS="="} $1=="uname" && $2=="test&pass" {print $1, $2, new_pass; next} {print}' $input_file > $output_file
 
-# Find the position of the password in the hex dump
-position=$(grep -ob 'uname=test&pass=' temp_hex_dump.txt | cut -d ":" -f 1)
-
-# Calculate the length of the password (in hex)
-password_length=$(echo -n "test" | xxd -p | tr -d '\n')
-
-# Replace the password in the hex dump
-sed -i "${position}s/${password_length}/$(echo -n "$new_password" | xxd -p | tr -d '\n')/g" temp_hex_dump.txt
-
-# Convert the hex dump back to binary
-xxd -r -p temp_hex_dump.txt > $output_file
-
-# Clean up temporary files
-rm temp_hex_dump.txt
-
-echo "Password changed successfully. Modified file: $output_file"
+echo "Password changed successfully to $new_password. Modified file: $output_file"
