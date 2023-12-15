b3e20baffec19e3be6f2b29c71563fd21ac0ee10
FrozenCave <timonsaueregger@gmail.com>
2023-12-15 05:43:47 -0500

    set env var

--- hidden/generate_flag.sh
+++ hidden/generate_flag.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# Generate a random UUID
-flag_uuid=$(uuidgen)
+echo "TH{$flag}" > goldnugget.txt
 
-# Create the flag with the desired format
-flag="TH{$flag_uuid}"
-
-# Create the text file with the generated flag
-echo "$flag" > goldnugget.txt
-
-# Specify the passphrase for steghide
-passphrase="Password"
+passphrase="password123"
 
 # Hide the text file in the image using steghide with the specified passphrase
 steghide embed -cf wood.jpg -ef goldnugget.txt --passphrase "$passphrase"
-# The "--passphrase" option is used to provide the passphrase directly to steghide.
 
-# Install nginx
 apt-get update
 apt-get install -y nginx
 
-# Set permissions for the files
 chmod 644 /app/wood.jpg /app/goldnugget.txt
 
-# Configure nginx to serve only wood.jpg and block access to goldnugget.txt
 echo "server {
   listen 80;
   server_name localhost;
@@ -40,5 +28,4 @@ echo "server {
   }
 }" > /etc/nginx/sites-available/default
 
-# Start nginx
 nginx -g 'daemon off;'
