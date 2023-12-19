052ba00783ea21a8accc72886c616dff97030ed7
FrozenCave <115781703+FrozenCave@users.noreply.github.com>
2023-12-19 11:17:12 +0100

    Update func.py

--- FileNigma/func.py
+++ FileNigma/func.py
@@ -5,7 +5,7 @@ import uuid
 
 flag = os.environ.get('FLAG', 'default_value')
 
-# Function to generate a string with 10,000 different letters, numbers, and signs with random paragraphs
+# Function to generate10,000 different letters
 def generate_large_random_string():
     content = ''
 
@@ -13,20 +13,18 @@ def generate_large_random_string():
         num_chars_in_paragraph = random.randint(100, 500)  # Adjust the range as needed
         content += ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(num_chars_in_paragraph))
         
-        # Add a paragraph after a random amount of characters
+       
         if len(content) < 10000 and random.choice([True, False]):
             content += '\n\n'
     
     return content
 
-# Directory to store the files inside the Docker container
 directory = '/usr/local/apache2/htdocs'
 
-# Create 100 files
+# File creation
 for i in range(1, 101):
     filename = os.path.join(directory, f'file_{i}.txt')
 
-    # For one file, use the specified format
     if i == 1:
         content = generate_large_random_string()
     else:
@@ -35,10 +33,8 @@ for i in range(1, 101):
     with open(filename, 'w') as file:
         file.write(content)
 
-# Choose a random file
 chosen_file = os.path.join(directory, random.choice([f for f in os.listdir(directory) if f.endswith('.txt')]))
 
-# Generate the content for the chosen file
 content = 'TH{' + flag + '}' + generate_large_random_string()
 
 # Write the content to a random location in the file
@@ -48,7 +44,6 @@ with open(chosen_file, 'r') as file:
     # Choose a random position to insert the content
     insert_position = random.randint(0, len(file_content))
 
-    # Insert the content at the chosen position
     updated_content = file_content[:insert_position] + content + file_content[insert_position:]
 
 # Write the updated content back to the file
