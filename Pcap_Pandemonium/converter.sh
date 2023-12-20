06ed07d9d5fdbf7ec06f25351a5185c507a77d03
Maximilian Birnbacher <66524685+maxbirnbacher@users.noreply.github.com>
2023-12-20 19:55:31 +0100

    Update converter.sh for pcap_pandemonium
    added dynamic flag

--- Pcap_Pandemonium/converter.sh
+++ Pcap_Pandemonium/converter.sh
@@ -4,12 +4,17 @@
 input_file="./input.pcapng"
 
 # Output file
-output_file="./capture.pcapng"
+output_file="/var/www/html/capture.pcapng"
+
+echo "Flag: $FLAG"
 
 # Generate a UUID with the format "TH{xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}"
-new_password="TH{$(python3 -c 'import uuid; print(str(uuid.uuid4()))' | tr -d '-' | tr '[:upper:]' '[:lower:]')}"
+new_password="TH{${FLAG}}"
 
 # Replace the password in the pcapng file and remove the trailing "test"
 awk -v new_pass="$new_password" 'BEGIN {FS=OFS="="} $1=="uname" && $2=="test&pass" {print $1, $2, new_pass; next} {print}' $input_file > $output_file
 
 echo "Password changed successfully to $new_password. Modified file: $output_file"
+
+# Start Apache in the foreground
+/usr/sbin/apache2ctl -D FOREGROUND
