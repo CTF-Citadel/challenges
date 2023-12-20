906309e70d90c22e3dff03cb3cb81cc1cffaa08e
FrozenCave <timonsaueregger@gmail.com>
2023-12-20 08:43:43 -0500

    Fixxed zip.py

--- zipception/zip.py
+++ zipception/zip.py
@@ -1,28 +1,41 @@
-import os, zipfile
+import os
+import zipfile
 
-env_variable = os.getenv("FLAG")  # Updated to use "flag" as the environment variable
+def zip_file(input_file, output_zip):
+    with zipfile.ZipFile(output_zip, 'w') as zipf:
+        zipf.write(input_file, arcname=os.path.basename(input_file))
 
-# Set the flag for the file content 
-flag = f"TH{{{env_variable}}}"
-
-
-
-# Fix this
-while True:
+def generate_zip_files(input_text_file, output_zip, num_iterations, flag):
     # Create the goldnugget.txt file
     file_content = flag.encode("utf-8")
-    with open("goldnugget.txt", "wb") as file:
+    with open(input_text_file, "wb") as file:
         file.write(file_content)
 
-    # Zip the file 100 times
-    for _ in range(100):
-        with zipfile.ZipFile("goldnugget.zip", "w") as zip_file:
-            zip_file.write("goldnugget.txt")
+    for i in range(num_iterations):
+        current_zip = f'zip{i}.zip'
+        zip_file(input_text_file, current_zip)
+        input_text_file = current_zip
 
-        # Update the content to be the zipped file
-        with open("goldnugget.txt", "rb") as file:
-            file_content = file.read()
+    # Move the final zip file to the desired name
+    os.rename(input_text_file, output_zip)
 
-        # Write the zipped content back to the file
-        with open("goldnugget.txt", "wb") as file:
-            file.write(file_content)
+    # Remove intermediate zip files
+    for i in range(num_iterations):
+        current_zip = f'zip{i}.zip'
+        if os.path.exists(current_zip):
+            os.remove(current_zip)
+
+def main():
+    env_variable = os.getenv("FLAG")  # Updated to use "flag" as the environment variable
+
+    # Set the flag for the file content
+    flag = f"TH{{{env_variable}}}"
+
+    input_text_file = 'goldnugget.txt'
+    output_zip = 'goldnugget.zip'
+    num_iterations = 9999
+
+    generate_zip_files(input_text_file, output_zip, num_iterations, flag)
+
+if __name__ == "__main__":
+    main()
